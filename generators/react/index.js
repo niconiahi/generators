64d706cf4a806ae661cf95ae28caf44e15404c42
niconiahi <nicolas.accetta@gmail.com>
2021-05-21 01:03:56 -0300

    fix: remove when lifecycle method

--- generators/react/index.js
+++ generators/react/index.js
@@ -1,16 +1,12 @@
 const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
-  async initialize() {
+  async prompting() {
     const answers = await this.prompt([
       {
         type: "list",
         name: "eslint",
         message: "ESLint for what language?",
-        when: (answers) => {
-        console.log('initialize ~ answers', answers)
-          return answers.features.includes("eslint")
-        },
         choices: [
           {
             name: "typescript",
@@ -19,8 +15,8 @@ module.exports = class extends Generator {
       },
     ]);
 
+    console.log('initialize ~ answers.eslint', answers.eslint)
     if (answers.eslint) {
-        console.log('initialize ~ answers.eslint', answers.eslint)
       if (answers.eslint.includes("typescript")) {
         this.composeWith(require.resolve("./eslint/typescript"));
       }
