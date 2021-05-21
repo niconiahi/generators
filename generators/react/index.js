859c6eecafb01764aa7d3cc5ee3f50b17af73247
niconiahi <nicolas.accetta@gmail.com>
2021-05-21 00:53:23 -0300

    console

--- generators/react/index.js
+++ generators/react/index.js
@@ -7,7 +7,10 @@ module.exports = class extends Generator {
         type: "list",
         name: "eslint",
         message: "ESLint for what language?",
-        when: (answers) => answers.features.includes("eslint"),
+        when: (answers) => {
+        console.log('initialize ~ answers', answers)
+          return answers.features.includes("eslint")
+        },
         choices: [
           {
             name: "typescript",
@@ -17,6 +20,7 @@ module.exports = class extends Generator {
     ]);
 
     if (answers.eslint) {
+        console.log('initialize ~ answers.eslint', answers.eslint)
       if (answers.eslint.includes("typescript")) {
         this.composeWith(require.resolve("./eslint/typescript"));
       }
